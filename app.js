f6e612b4d830bf8ce6020e6cc824dfe5c65b4e5e
Vincent Richitelli <vin.richitelli@gmail.com>
2021-03-31 23:52:19 -0400

    update

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 // Node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require('util');
-
+const util = require("util");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-
 // Lib modules
 const timestamp = require("./lib/timestamp");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-
 const validate = {
     required: input => input !== '' ? true : "This field is required.",
     name: input => input !== '' ? true : "Please enter a name.",
     id: input => Number.isInteger(Number(input)) && Number(input) > 0 ? true : "Please enter a positive whole number.",
     email: input => input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi) ? true : "Please enter a valid email address."
 }
-
 const questions = {
     type: function() {
         return {
@@ -37,9 +33,7 @@ const questions = {
         }
     }
 };
-
 let employees = [];
-
 async function addRole(member) {
     let { name } = await inquirer.prompt(questions.item(member, "name", "full name", validate.name));
     let { id } = await inquirer.prompt(questions.item(member, "id", "ID number", validate.id));
@@ -59,11 +53,9 @@ async function addRole(member) {
             break;
     }
 }
-
 function getHTMLModule(file) {
     return readFile(file, "utf8");
 }
-
 async function generateHTML() {
     let Template = {
         Main: await getHTMLModule("./templates/main.html"),
@@ -71,32 +63,37 @@ async function generateHTML() {
         Engineer: await getHTMLModule("./templates/engineer.html"),
         Intern: await getHTMLModule("./templates/intern.html")
     }
-
     let employeesHTML = "";
 
     for (let employee of employees) {
         let html = Template[employee.constructor.name]
+        .replace("{% name %}", employee.name)
+        .replace("{% id %}", employee.id)
+        .replace("{% email %}", employee.email);
         .replace(/{% name %}/gi, employee.name)
         .replace(/{% id %}/gi, employee.id)
         .replace(/{% email %}/gi, employee.email);
         switch (employee.constructor.name) {
             case "Manager":
+                html = html.replace("{% officeNumber %}", employee.officeNumber);
                 html = html.replace(/{% officeNumber %}/gi, employee.officeNumber);
                 break;
             case "Engineer":
+                html = html.replace("{% github %}", employee.github);
                 html = html.replace(/{% github %}/gi, employee.github);
                 break;
             case "Intern":
+                html = html.replace("{% school %}", employee.school);
                 html = html.replace(/{% school %}/gi, employee.school);
                 break;
         }
         employeesHTML += html;
     }
+    let completeHTML = Template["Main"].replace("{% employees %}", employeesHTML);
     let completeHTML = Template["Main"].replace(/{% employees %}/gi, employeesHTML);
 
     createHTML(completeHTML);
 }
-
 async function createHTML(html) {
     console.log("Creating HTML...");
     let file = `team-${timestamp()}.html`;
@@ -108,7 +105,6 @@ async function createHTML(html) {
     console.log(`HTML has been created to "${dir}/${file}".`);
     return;
 }
-
 async function init() {
     console.log("Please build your team");
     await addRole("Manager");
@@ -122,5 +118,9 @@ async function init() {
         await addRole(member);
     }
 }
-
 init();
+
+function replace (arg0, name)
+{
+    throw new Error("Function not implemented.");
+}
